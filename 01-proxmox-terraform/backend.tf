c1ab26760d361ca19579f3bdd58a3ec6fd507560
Facundo Tolay <facundo.tolay@mikroways.net>
2026-09-21 19:48:56 -0300

    fix: s3 terraform backend

--- 01-proxmox-terraform/backend.tf
+++ 01-proxmox-terraform/backend.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "nerdearla-workshop-097096559085-us-east-1-an"
+    key    = "01-proxmox-terraform.tfstate"
+    region = "us-east-1"
+  }
 }
